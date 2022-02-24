56704fb5ac712f90ecf78faca1f5abf39ab9997d
m.usman <musman.developer@gmail.com>
2022-02-24 19:20:13 +0500

    Post request (Instructor's way)

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,3 +1,12 @@
-class API{
-  
+import 'package:covid_2019_rest_api/app/services/api_keys.dart';
+
+class API {
+  API({required this.apiKey});
+  final String apiKey;
+
+  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+  static String host = "https://ncov2019-admin.firebaseapp.com";
+  String tokenUri = '$host/token';
+
+  // Uri tockenUri() => Uri(scheme: 'https', host: host, path: 'token');
 }
